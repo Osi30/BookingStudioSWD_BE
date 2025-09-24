6a452b1cc39da10ada2c5d76d53431f93abf10b7
Osi30 <nthanhn2491@gmail.com>
2025-09-24 22:10:19 +0700

    [UPDATE] Update Env Loader

--- src/main/java/com/studio/booking/utils/DotenvLoader.java
+++ src/main/java/com/studio/booking/utils/DotenvLoader.java
@@ -1,12 +1,17 @@
 package com.studio.booking.utils;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 
 public class DotenvLoader {
     public static void loadEnv() {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        try {
+            Dotenv dotenv = Dotenv.configure().load();
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+        } catch (DotenvException e) {
+            System.out.println("This is not an error because env will be loaded by third party");
+        }
     }
 }
