d79b028d872a6b2763ab9a98977709ab2c12b658
Osi <nthanhn2491@gmail.com>
2025-10-01 21:05:51 +0700

    [UPDATE] Update AppConfig.java

--- src/main/java/com/studio/booking/configs/AppConfig.java
+++ src/main/java/com/studio/booking/configs/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -41,6 +42,7 @@ public class AppConfig {
     }
 
     @Bean
+    @Profile("!test & !ci")
     ApplicationRunner runner() {
         return args -> {
             // Create account admin
