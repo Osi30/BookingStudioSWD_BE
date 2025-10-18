893fef7d6ff400c842e92a6d10ea7f2454c430eb
Osi30 <nthanhn2491@gmail.com>
2025-10-18 18:58:00 +0700

    [FEAT] Add cron job for update price table

--- src/main/java/com/studio/booking/repositories/PriceTableRepo.java
+++ src/main/java/com/studio/booking/repositories/PriceTableRepo.java
@@ -3,8 +3,11 @@ package com.studio.booking.repositories;
 import com.studio.booking.entities.PriceTable;
 import com.studio.booking.enums.PriceTableStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PriceTableRepo extends JpaRepository<PriceTable, String> {
@@ -20,5 +23,31 @@ public interface PriceTableRepo extends JpaRepository<PriceTable, String> {
             OR pt.status = com.studio.booking.enums.PriceTableStatus.IS_HAPPENING
             """)
     List<PriceTable> findAllByStudioType(String studioTypeId);
+
+    // Thao tác thay đổi dữ liệu
+    // Thực hiện xóa Cache lỗi thời
+    @Modifying(clearAutomatically = true)
+    // Để thực hiện thao tác
+    @Transactional
+    @Query("""
+            UPDATE PriceTable pt
+            SET pt.status = com.studio.booking.enums.PriceTableStatus.IS_HAPPENING
+            WHERE pt.status = com.studio.booking.enums.PriceTableStatus.COMING_SOON
+            AND pt.startDate = :date
+            """)
+    int updateStatusIsReadyNow(LocalDate date);
+    // Số lượng bản ghi đã cập nhập
+
+    @Modifying(clearAutomatically = true)
+    // Để thực hiện thao tác
+    @Transactional
+    @Query("""
+            UPDATE PriceTable pt
+            SET pt.status = com.studio.booking.enums.PriceTableStatus.ENDED
+            WHERE pt.status = com.studio.booking.enums.PriceTableStatus.IS_HAPPENING
+            AND pt.endDate = :date
+            """)
+    int updateStatusIsEndNow(LocalDate date);
+    // Số lượng bản ghi đã cập nhập
 }
 
