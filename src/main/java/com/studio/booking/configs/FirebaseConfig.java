661fb97a7f35abab1c14cf147adf42fd1a752798
Osi <nthanhn2491@gmail.com>
2025-10-28 20:40:17 +0700

    [FIX] Add profile annotation to FirebaseConfig class

--- src/main/java/com/studio/booking/configs/FirebaseConfig.java
+++ src/main/java/com/studio/booking/configs/FirebaseConfig.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import com.studio.booking.exceptions.exceptions.MessagingException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
 
+@Profile("!ci && !test")
 @Configuration
 public class FirebaseConfig {
     @Value("${FIREBASE_ADMIN_BASE64}")
