778c65fa3d38ff5a51273fc9b590bf67bfc42b0f
Osi30 <nthanhn2491@gmail.com>
2025-11-11 17:35:45 +0700

    [FEAT] Add momo payment

--- src/main/java/com/studio/booking/entities/Booking.java
+++ src/main/java/com/studio/booking/entities/Booking.java
@@ -1,8 +1,8 @@
 package com.studio.booking.entities;
 
-import com.studio.booking.enums.BookingStatus;
-import com.studio.booking.enums.BookingType;
+import com.studio.booking.enums.*;
 import com.studio.booking.utils.GenerateUtil;
+import com.studio.booking.utils.Validation;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -65,5 +65,60 @@ public class Booking {
     @PrePersist
     public void generateId() {
         this.id = GenerateUtil.generateRandomWords(10);
+    }
+
+    public Double getRefundPrice() {
+        return this.studioAssigns.stream()
+                .filter(s -> s.getStatus().equals(AssignStatus.AWAITING_REFUND))
+                .mapToDouble(s -> s.getStudioAmount() + s.getServiceAmount())
+                .sum();
+    }
+
+    public Boolean isComplete() {
+        return isCompleteAssign() && isCompletePayment();
+    }
+
+    private Boolean isCompletePayment() {
+        return isPaymentTypeCompleted(PaymentType.REFUND_PAYMENT)
+                && isPaymentTypeCompleted(PaymentType.ADDITION_PAYMENT)
+                && isCustomerCompletePayment();
+    }
+
+    private Boolean isCustomerCompletePayment() {
+        // Case for pay full
+        if (this.bookingType.equals(BookingType.PAY_FULL)) {
+            // Have one payment full success
+            Payment payment = this.payments.stream()
+                    .filter(p -> p.getPaymentType().equals(PaymentType.FULL_PAYMENT)
+                            && p.getStatus().equals(PaymentStatus.SUCCESS))
+                    .findFirst().orElse(null);
+            return payment != null;
+        // Case for pay deposit
+        } else {
+            // Have one deposit and one final payment
+            List<Payment> payments = this.payments.stream()
+                    .filter(p -> p.getStatus().equals(PaymentStatus.SUCCESS)
+                            && (p.getPaymentType().equals(PaymentType.DEPOSIT)
+                            || p.getPaymentType().equals(PaymentType.FULL_PAYMENT)))
+                    .toList();
+            return payments.size() == 2;
+        }
+    }
+
+    private Boolean isPaymentTypeCompleted(PaymentType paymentType) {
+        List<Payment> unSuccessPayments = this.payments
+                .stream().filter(p -> p.getPaymentType().equals(paymentType)
+                        && !p.getStatus().equals(PaymentStatus.SUCCESS))
+                .toList();
+        return !Validation.isValidCollection(unSuccessPayments);
+    }
+
+    private Boolean isCompleteAssign() {
+        List<StudioAssign> assigns = this.studioAssigns
+                .stream().filter(s -> s.getStatus().equals(AssignStatus.AWAITING_REFUND)
+                        || s.getStatus().equals(AssignStatus.IS_HAPPENING))
+                .toList();
+
+        return !Validation.isValidCollection(assigns);
     }
 }
